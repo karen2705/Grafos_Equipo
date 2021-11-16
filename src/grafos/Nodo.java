e103f9700a54969d59f4848f352d60e7233dc180
Karen Vázquez <Karen Vzquez@192.168.0.125>
2021-11-16 01:11:34 -0600

    complete la clase Nodo

--- src/grafos/Nodo.java
+++ src/grafos/Nodo.java
@@ -4,10 +4,46 @@
  */
 package grafos;
 
-/**
- *
- * @author Karen Vázquez
- */
+import java.awt.Graphics;
+
 public class Nodo {
+    private int x,y;
+    public static final int d=60;
     
-}
+    public Nodo(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void pintar(Graphics g){
+        g.drawOval(this.x- d/2, this.y - d/2, d, d);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
