2e3c888cf49eca94b152f4b5ccb7965563cdfc4a
Karen Vázquez <Karen Vzquez@192.168.0.125>
2021-11-17 00:40:56 -0600

    Añadi la clase Grafos a la rama master

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -1,8 +1,10 @@
-/*
+  /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package grafos;
+
+import javax.swing.JFrame;
 
 /**
  *
@@ -15,6 +17,11 @@ public class Grafos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrame ventana = new JFrame("GRAFO");
+        ventana.add(new Lienzo());
+        ventana.setSize(600,600);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
     }
     
 }
