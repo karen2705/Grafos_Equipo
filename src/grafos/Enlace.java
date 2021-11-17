d6ac29dfe9c013798f63cfc39470e6a392cab6ac
Karen Vázquez <Karen Vzquez@192.168.0.125>
2021-11-16 21:15:29 -0600

    Modifique la clase enlace

--- src/grafos/Enlace.java
+++ src/grafos/Enlace.java
@@ -13,16 +13,33 @@ import java.awt.Graphics;
 public class Enlace {
     
     private int x1,y1, x2,y2;
+    private String nombre;
     
-    public Enlace(int x1, int y1, int x2, int y2){
+    public Enlace(int x1, int y1, int x2, int y2, String nombre){
         this.x1=x1;
         this.y1=y1;
         this.x2=x2;
         this.y2=y2;
+        this.nombre= nombre;
+        
       
     }
      public void pintar(Graphics g){
         g.drawLine(x1, y1,x2,y2);
+        if(x1> x2 && y1>y2){
+            g.drawString(nombre, x1 - Math.abs((x1 - x2)/2), y1 - Math.abs((y1 - y2)/2));
+        }
+         if(x1< x2 && y1 < y2){
+         g.drawString(nombre, x2 - Math.abs((x1 - x2)/2), y2 - Math.abs((y1 - y2)/2));
+          
+         }
+          if(x1 > x2 && y1 < y2){
+            g.drawString(nombre, x1 - Math.abs((x1 - x2)/2), y2 - Math.abs((y1 - y2)/2));
+        }
+           if(x1 < x2 && y1 > y2){
+            g.drawString(nombre, x2 - Math.abs((x1 - x2)/2), y1 - Math.abs((y1 - y2)/2));
+        }
+           
     }
 
     public int getX1() {
