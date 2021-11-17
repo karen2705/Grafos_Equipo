221472b83af5116c7c82d0f01b240a1ec1e86f07
Karen Vázquez <Karen Vzquez@192.168.0.125>
2021-11-16 21:18:26 -0600

    Modifique la clase Nodo

--- src/grafos/Nodo.java
+++ src/grafos/Nodo.java
@@ -8,15 +8,18 @@ import java.awt.Graphics;
 
 public class Nodo {
     private int x,y;
+    private String nombre;
     public static final int d=60;
     
-    public Nodo(int x, int y){
+    public Nodo(int x, int y, String nombre){
         this.x = x;
         this.y = y;
+        this.nombre= nombre;
     }
     
     public void pintar(Graphics g){
         g.drawOval(this.x- d/2, this.y - d/2, d, d);
+        g.drawString(nombre, x, y);
     }
 
     public int getX() {
